9f24da14d0a3a0ab71f3261ab92156aa05148ffd
halil <halil@DESKTOP-38P5TGT>
2021-02-18 03:55:26 +0300

    Add project files.

---
+++ MyList.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Genericıntro
+{
+    class MyList<T>
+    {
+         T[] items;
+
+         public MyList()
+         {
+             items = new T[0];
+         }
+
+         public void Add(T item)
+         {
+             T[] tempArray = items;
+             items = new T[items.Length+1];
+             int i = 0;
+             foreach (var temp in tempArray)
+             {
+                 items[i] = temp;
+                 i++;
+             }
+
+             items[items.Length - 1] = item;
+
+
+
+
+
+
+
+
+         }
+
+         public int Lenght
+         {
+             get { return items.Length; }
+         }
+
+         public T[] Items
+         {
+             get { return items; }
+         }
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Genericıntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            MyList<string> list = new MyList<string>();
+
+            list.Add("halil");
+
+            Console.WriteLine(list.Lenght);
+
+            list.Add("can");
+
+            Console.WriteLine(list.Lenght);
+
+            foreach (var listItem in list.Items)
+            {
+                Console.WriteLine(listItem);
+            }
+
+
+
+
+
+
+        }
+    }
+}
